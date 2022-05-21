db338767ec617f8c5b4950d094b0fb1ffe4837fd
GreyGooseVX <total.egal1234@web.de>
2022-05-21 12:45:16 +0200

    Update fextralife, block stream.js

--- Scripts/fextralife, block stream.js
+++ Scripts/fextralife, block stream.js
@@ -1,14 +1,14 @@
 // ==UserScript==
 // @name         fextralife, block stream
 // @description  block the twitch stream on fextralife
-// @version      1.3
+// @version      1.4
 // @author       Tobias
 // @match        *.wiki.fextralife.com/*
 // @license      GNU
 // @namespace    https://github.com/GreyGooseVX/Greasy-Fork-Scripts
 // ==/UserScript==
 
-let fextraStream = document.getElementById("fextravideo")
+let fextraStream = document.querySelector("#sidebar-wrapper .sidebar-nav #fextravideo")
 if (fextraStream != null) {
 	fextraStream.remove()
 }
