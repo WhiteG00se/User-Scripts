8da87ca51aa4303b7bbf117bdb3849f7c4d226b2
GreyGooseVX <total.egal1234@web.de>
2022-10-16 00:17:05 +0200

    page was updated, fixing script

--- scripts/stockanalysis, improve layout.user.js
+++ scripts/stockanalysis, improve layout.user.js
@@ -1,11 +1,13 @@
 // ==UserScript==
 // @name         stockanalysis, improve layout
 // @description  improves the layout of some tables to show more columns at once
-// @version      2.0
+// @version      2.1
 // @author       Tobias L
 // @match        https://stockanalysis.com/stocks/*/financials*
 // @license      GPL-3.0-only
 // @namespace    https://github.com/WhiteG00se/User-Scripts
 // ==/UserScript==
- 
-document.querySelector('#main > div.table-wrap.svelte-17fayh1').className = ''
+
+setTimeout(function () {
+	document.querySelector('#main > div.table-wrap.svelte-17fayh1').className = ''
+}, 200)
